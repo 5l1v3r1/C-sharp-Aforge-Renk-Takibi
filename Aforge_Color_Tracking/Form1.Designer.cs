08d82b80cbd012760108279040161fba884bfd4b
Osman Yavuz <58117722+OsmanYavuz-web@users.noreply.github.com>
2020-03-12 12:44:55 +0300

    Initial commit

---
+++ Aforge_Color_Tracking/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Aforge_Color_Tracking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(34, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cam Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(235, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1. renk";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(234, 97);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2. renk";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(711, 430);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(28, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Cam Durdur";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(235, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(281, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(327, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(40, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(235, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(40, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(281, 66);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(40, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(327, 66);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(40, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(235, 120);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(40, 20);
+            this.textBox7.TabIndex = 16;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(281, 120);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(40, 20);
+            this.textBox8.TabIndex = 15;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(327, 120);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(40, 20);
+            this.textBox9.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(373, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "RGB";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(373, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "RGB";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(373, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "HSB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 601);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Aforge_Color_Tracking/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//Aforge ile Uzaylar
+using AForge;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision;
+using AForge.Vision.Motion;
+
+namespace Aforge_Color_Tracking
+{
+    public partial class Form1 : Form
+    {
+        //Siniflarin nesneleri tanimlaniyor
+        private VideoCaptureDevice FinalVideoSource;
+        private FilterInfoCollection VideoCaptuerDevices;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Sisteme bagli olan Cam listesini aliyoruz
+            VideoCaptuerDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo VideoCaptureDevice in VideoCaptuerDevices)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name); // WebCamlarin hepsi ComboBox'da listeleniyor
+                comboBox1.SelectedIndex = 0;  //1.User im Combobox wird markiert.
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FinalVideoSource = new VideoCaptureDevice(VideoCaptuerDevices[comboBox1.SelectedIndex].MonikerString);//1.User ist gewählt
+            FinalVideoSource.NewFrame += new NewFrameEventHandler(FinalVideoSource_NewFrame);
+
+            FinalVideoSource.Start();
+        }
+
+        void FinalVideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap image1 = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = image;
+
+            try
+            {
+
+                //kırmızı
+                if (radioButton1.Checked)
+                {
+                    //ColorFilter'in yapildigi yer
+                    ColorFiltering filter = new ColorFiltering();
+                    int a1 = int.Parse(textBox1.Text);
+                    int a2 = int.Parse(textBox2.Text);
+                    int a3 = int.Parse(textBox3.Text);
+                    int b1 = int.Parse(textBox4.Text);
+                    int b2 = int.Parse(textBox5.Text);
+                    int b3 = int.Parse(textBox6.Text);
+                    filter.Red = new IntRange(a1, b1);
+                    filter.Green = new IntRange(a2, b2);
+                    filter.Blue = new IntRange(a3, b3);
+                    filter.ApplyInPlace(image1);
+
+                    //Görüntü üzerinde algilanan renk kare icine aliniyor
+                    cevresiniciz(image1);
+
+                }
+
+                //mavi
+                if (radioButton2.Checked)
+                {
+                    int a1 = int.Parse(textBox7.Text);
+                    int a2 = int.Parse(textBox8.Text);
+                    int a3 = int.Parse(textBox9.Text);
+                    // Euclidean Color Filterin yapildigi yer
+                    EuclideanColorFiltering filter = new EuclideanColorFiltering();
+                    // Algilanacak renk belirleniyor ve orta noktasi belirleniyor
+                    filter.CenterColor = new RGB(Color.FromArgb(a1, a2, a3));
+                    filter.Radius = 50;
+                    // filtre calistiriliyor
+                    filter.ApplyInPlace(image1);
+
+                    //Görüntü üzerinde algilanan rengi cevrcevelemek veya hedeflemek icin gerekli Methodlar
+                    cevresiniciz(image1);
+                    //hedefal(image);
+                }
+            }
+            catch { }
+
+        }
+        /////////Burdan sonra ekrandaki color etrafina Dikdörtgen cizdiriyoruz/////////////
+        public void cevresiniciz(Bitmap image)
+        {
+
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.MinWidth = 2;
+            blobCounter.MinHeight = 2;
+            blobCounter.FilterBlobs = true;
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
+
+            Grayscale grayFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap grayImage = grayFilter.Apply(image);
+
+            blobCounter.ProcessImage(grayImage);
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();
+            foreach (Rectangle recs in rects)
+            {
+
+                if (rects.Length > 0)
+                {
+                    Rectangle objectRect = rects[0];
+                    //Graphics g = Graphics.FromImage(image);
+                    Graphics g = pictureBox1.CreateGraphics();
+                    using (Pen pen = new Pen(Color.FromArgb(0, 0, 0), 2))
+                    {
+
+                        g.DrawRectangle(pen, objectRect);
+                    }
+
+                    //Cizdirilen Dikdörtgenin Koordinatlari aliniyor.
+                    int objectX = objectRect.X + (objectRect.Width / 2);
+                    int objectY = objectRect.Y + (objectRect.Height / 2);
+                    //int objectX = objectRect.X;
+                    //int objectY = objectRect.Y;
+                    g.DrawString(objectX.ToString() + "X" + objectY.ToString(), new Font("Arial", 12), Brushes.Red, new System.Drawing.Point(250, 1));
+                    g.Dispose();
+
+                }
+            }
+        }
+        ///////////////////// Buraya kadar //////////////////////////////////////////
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Webcam beim Schließen des Programms wieder freigeben
+            if (FinalVideoSource != null && FinalVideoSource.IsRunning)
+            {
+                FinalVideoSource.SignalToStop();
+                FinalVideoSource = null;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Webcam beim Schließen des Programms wieder freigeben
+            if (FinalVideoSource != null && FinalVideoSource.IsRunning)
+            {
+                FinalVideoSource.SignalToStop();
+                FinalVideoSource = null;
+
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
